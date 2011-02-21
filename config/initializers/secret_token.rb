945a40f9e40c3c5436955711e8bb1f041c401ef8
Recardo <webmaster@recardo-el-bonito.com>
2011-02-21 20:55:10 +0100

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subtools::Application.config.secret_token = '803bb278d9acda99ecefc4b17e51fc133e53575c7c63bae10d643eea12caaecaafb804b97f54c317f6705fd549e0cd4044962b8b97d6969ebe52fbcfe78b4d0c'
